6bbb033625ada615656503dc32d4e4138783b074
owenzhang <owen@owenzhang.com>
2022-01-20 10:57:40 +0800

    onclick

---
+++ assets/js/owen.169f7c1f.js
@@ -0,0 +1,11 @@
+<script>
+    var oBtn = document.getElementById("btn_svg_header");
+    oBtn.onclick = function () {
+        var menuClass = document.getElementById('show_menu').className;
+        if(menuClass=='mobile-menu-wrapper'){
+             oBtn.className ='mobile-menu-wrapper open';
+        }else{
+            oBtn.className ='mobile-menu-wrapper';
+        }
+    }
+</script>
